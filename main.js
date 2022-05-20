57d2e4355c276c826c12e19d9c26b1459e1e8250
Maria Gabrielly <ggabrielly.ggaby@gmail.com>
2022-05-19 21:51:41 -0300

    Exercicio_js

---
+++ main.js
@@ -0,0 +1,40 @@
+//Semáforo
+function trafficLight() {
+    if(document.getElementById('red_light').value=='Vermelho') {
+        document.getElementById('img_traffic_light').src="img/red_light.png";
+    }
+    else if(document.getElementById('yellow_light').value=='Amarelo') {
+        document.getElementById('img_traffic_light').src="img/yellow_light.png";
+    }
+    else {
+        document.getElementById('img_traffic_light').src="img/all_off_light.png";
+    }
+}
+
+//CelsiusparaFahrenheit
+function toFahrenheit() {
+    const C = document.getElementById('inputCelcius').value;
+    Number(C);
+    const F = C*1.8+32;
+    document.getElementById('inputFahrenheit').value=F;
+}
+
+//FahrenheitparaCelsius
+function toCelcius() {
+    const F = document.getElementById('inputFahrenheit').value;
+    Number(F);
+    const C = (F-32)/1.8;
+    document.getElementById('inputCelcius').value=C;
+}
+
+//CalcularIMC
+function imc() {
+    const altura = document.getElementById('inputAltura').value;
+    Number(altura);
+    const peso = document.getElementById('inputPeso').value;
+    Number(peso);
+    const IMC = peso/(altura*altura);
+    document.getElementById('inputIMC').value=IMC;
+}
+
+//TabelaCarros
